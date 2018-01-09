6dba23d818e6185a1cc4a6109a66fb440ffdbe31
godfredcs <addaiz.godfred@gmail.com>
2018-01-09 18:06:41 +0000

    Created weather actionCreator

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(reduxPromise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/weather_reducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+    city: ''
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
